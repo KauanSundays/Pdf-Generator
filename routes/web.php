f1cfedaf30ffc9fe52afb9c24ca32f813c342e71
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-09-19 14:48:58 -0300

    feat: wip

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\PdfController;
 use Illuminate\Support\Facades\Route;
 use Barryvdh\DomPDF\Facade\Pdf;
 
-$pdf = Pdf::loadView('pdf.invoice', $data);
-return $pdf->download('invoice.pdf');
 
 Route::get('/', function () {
-    $pdf = Pdf::loadView('pdf.invoice', $data);
+
+    $pdf = Pdf::loadView('pdf.invoice');
     return $pdf->download('invoice.pdf');
-})
+    
+});
